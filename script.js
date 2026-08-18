eeb82d930fe232466d27ab0010ef54c740d04ef7
Byron-bs <jmlgroupsnc@gmail.com>
2026-08-18 15:50:08 +0200

    Completa le foto dei primi piatti

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const productImages = {
 
 let menuData = null;
 
-fetch("menu.json?v=20260818-primi1")
+fetch("menu.json?v=20260818-primi2")
 .then(response => response.json())
 .then(menu => {
     menuData = menu;
